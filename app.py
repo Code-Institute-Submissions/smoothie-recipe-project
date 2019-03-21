8d05004fe31ccde42a527bc3ee2ab89eee4ea581
Heather Caputo <caputoheather@ymail.com>
2019-03-21 15:17:35 +0000

    fixed bug in ingredient/method steps that was preventing edit recipe working

--- app.py
+++ app.py
@@ -58,22 +58,22 @@ def get_spring():
     recipes=mongo.db.recipes.find({"season" : "spring"}))
     
 
-@app.route('/recipes_by_season/<season>', methods=["POST"])
-def recipes_by_season(season):
-    """
-    Get all recipes of a chosen category and display
-    these recipes on one page
-    """
-    # Counts total amount of chosen category recipes
-    recipes_total = mongo.db.recipes.find({
-        "season": season
-    }).count()
-    return render_template(
-        "results.html",
-        recipes=mongo.db.recipes.find({"season": season}),
-        recipe=mongo.db.recipes.find(),
-        season=season,
-        recipes_total=recipes_total)
+# @app.route('/recipes_by_season/<season>', methods=["POST"])
+# def recipes_by_season(season):
+#     """
+#     Get all recipes of a chosen category and display
+#     these recipes on one page
+#     """
+#     # Counts total amount of chosen category recipes
+#     recipes_total = mongo.db.recipes.find({
+#         "season": season
+#     }).count()
+#     return render_template(
+#         "results.html",
+#         recipes=mongo.db.recipes.find({"season": season}),
+#         recipe=mongo.db.recipes.find(),
+#         season=season,
+#         recipes_total=recipes_total)
         
 
 
@@ -129,7 +129,7 @@ def search():
 @app.route('/insert_recipe', methods=["POST"])
 def insert_recipe():
     recipe=mongo.db.recipes
-    ingreds=request.form.getlist('ingredients')
+    ingreds_req=request.form.getlist('ingredients')
     method_steps=request.form.getlist('method')
     diet_req=request.form.getlist('dietary_requirement_type')
     recipe_details = {
@@ -139,7 +139,7 @@ def insert_recipe():
         'time_taken': request.form['time_taken'],
         'season':request.form['season'],
         'difficulty_rating':request.form['difficulty_rating'],
-        'ingredients': ingreds,
+        'ingredients': ingreds_req,
         'method':method_steps,
         'star_rating_value':request.form['star_rating_value'],
         'dietary_requirement_type':diet_req
